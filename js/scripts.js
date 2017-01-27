811f19b017ecaa27f11f21061377dd09a5bd0aef
Jayeson Kiyabu <jkiyabu@comcast.net>
2017-01-27 09:13:15 -0800

    add function to display size and 1 topping selected back to user

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   $("#pizza-order").submit(function(event) {
     event.preventDefault();
     var size = ($("select#size").val()).toLowerCase();
+    var topping = [$("input:checkbox[name=topping]:checked").val()];
+
+    $(".topping").append("<li>" + topping + "</li>");
     $(".size-order").append(size);
     $("#price").show;
   });
